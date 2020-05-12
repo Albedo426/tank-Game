c0d28082b696be01f1e03f4e1ce55b5a34943210
Albedo426 <65228887+Albedo426@users.noreply.github.com>
2020-05-12 19:22:35 +0300

    Add files via upload

---
+++ tankcalismasi/tankcalismasi/Form1.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace tankcalismasi
+{
+    public partial class Form1 : Form
+    {
+        Recrangel[] re = new Recrangel[10];
+        gun guner;
+        public Form1()
+        {
+            this.SetStyle(
+               System.Windows.Forms.ControlStyles.UserPaint |
+               System.Windows.Forms.ControlStyles.AllPaintingInWmPaint |
+               System.Windows.Forms.ControlStyles.OptimizedDoubleBuffer,
+               true);
+            InitializeComponent();
+            re[0] = new Recrangel(500, 500);
+            Random r = new Random();
+            for (int i=1;i< enemynumber; i++) {
+                re[i] = new Recrangel(r.Next(0, 500), r.Next(0, 500));
+            }
+            timer1.Dispose();
+            timer1.Start();
+            //Recrangel
+            Paint += Form1_Paint;
+            KeyDown += Form1_KeyDown;
+        }
+        int enemynumber =3;
+
+        private void guncrative(object sender, PaintEventArgs e)
+        {
+            re[0].bullet[0].drawRecrangel(e);
+            if (Math.Abs(re[0].bullet[0].x) == 800 || Math.Abs(re[0].bullet[0].y) == 800)
+            {
+                  timer.Dispose();
+                Cansqueeze = true;
+            }
+        }
+        bool Cansqueeze = true;
+        private void gunmoving(object sender, EventArgs e)
+        {
+            re[0].bullet[0].gunmov();
+            for (int i =1;i<enemynumber;i++) {
+                if ((re[0].bullet[0].x - 10) < re[i].x && re[i].x < (re[0].bullet[0].x + 10) && (re[0].bullet[0].y - 10) < re[i].y && re[i].y < (re[0].bullet[0].y + 10))
+                {
+                    re[i].islive = false;
+                }
+            }
+            Refresh();
+            Application.DoEvents();
+
+        }
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyValue==(int)Keys.Space) {
+                if (Cansqueeze) {
+                    re[0].bullet[0] = null;
+                    guner = new gun(re[0].x, re[0].y, 10);
+                    Paint += guncrative;
+                    re[0].setbullet(guner);
+                    re[0].bullet[0].gunmove(re[0]);
+                    timer.Dispose();
+                    timer.Start();
+                    Cansqueeze = false;
+                }
+            }
+            re[0].movekeylistener(e);
+            Refresh();
+            Application.DoEvents();
+
+        }
+      
+        public void enemygun() {
+          
+            re[gunkey].bullet[0] = null;
+            guner = new gun(re[gunkey].x, re[gunkey].y, 10);
+            Paint += enemygundraw; 
+            re[gunkey].setbullet(guner);
+            re[gunkey].bullet[0].gunmove(re[gunkey]);
+            timer2.Dispose();
+            timer2.Start();
+            
+        }
+        int firestart = 1;
+        private void enemygundraw(object sender, PaintEventArgs e)
+        {
+          
+            if (Math.Abs(re[gunkey].bullet[0].x) > 800 || Math.Abs(re[gunkey].bullet[0].y) > 800 || Math.Abs(re[gunkey].bullet[0].x) <0 || Math.Abs(re[gunkey].bullet[0].y) < 0 )
+            {
+                timer2.Stop();
+                Cansqueeze = true;
+                firestart = 1;
+            }
+            re[gunkey].bullet[0].drawRecrangel(e);
+
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            re[0].drawRecrangel(e);
+            for (int i = 1; i < enemynumber; i++) {
+                if (re[i].islive) {
+                    re[i].drawRecrangel(e);
+                }
+            }
+        }
+        int gunkey = 0;
+        private void enemymove(object sender, EventArgs e)
+        {
+            for (int i = 1; i < enemynumber; i++)
+            {
+                re[i].randmove();
+                if ((re[i].x - 10) < re[0].x && re[0].x < (re[i].x + 10)|| (re[i].y - 10) < re[0].y && re[0].y < (re[i].y + 10) && re[i].islive)
+                {
+                    if (firestart==1 && re[i].islive) {
+                        gunkey = i;
+                        enemygun();
+                        firestart = 0;
+                    }
+                }
+            }
+            Refresh();
+            Application.DoEvents();
+
+        }
+
+        private void enemygunmove(object sender, EventArgs e)
+        {
+            re[gunkey].bullet[0].gunmov();
+            if( (re[gunkey].bullet[0].x - 10) < re[0].x && re[0].x < (re[gunkey].bullet[0].x + 10) && (re[gunkey].bullet[0].y - 10) < re[0].y && re[0].y < (re[gunkey].bullet[0].y + 10) && re[gunkey].islive)
+            {
+                Application.Exit();
+            }
+            Refresh();
+            Application.DoEvents();
+         
+        }
+    }
+}
---
+++ tankcalismasi/tankcalismasi/gun.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace tankcalismasi
+{
+    class gun
+    {
+
+        public int x=0, y=0,v=0;
+      
+        public double dx = 0;
+        public double dy = 0;
+        
+
+
+        public gun(int cx ,int cy,int cv) {
+            x = cx;
+            y = cy;
+            v = cv;
+        }
+        public void drawRecrangel(PaintEventArgs e)
+        {
+
+            SolidBrush blueBrush = new SolidBrush(Color.Blue);
+            Rectangle rect = new Rectangle(x+7, y+7, 4, 4);
+            e.Graphics.FillEllipse(blueBrush, rect);
+        }
+        public void gunmov()
+        {
+            x += (int)dx;
+            y += (int)dy;
+        }
+        public void gunmove(Recrangel r) {
+           
+            if (r.yon == "Right")
+            {
+                dx = +v;
+                dy = 0;
+            }
+            else if (r.yon == "Left")
+            {
+                dx = -v;
+                dy = 0;
+            }
+            else if (r.yon == "Up")
+            {
+                dx = 0;
+                dy = -v;
+            }
+            else if (r.yon == "Down")
+            {
+                dx = 0;
+                dy = v;
+            }
+        }
+
+        
+        //mouse icin
+        //public void gunmove(Recrangel r) {
+
+        //    xpos = r.x;
+        //    ypos = r.y;
+        //    x = (int)xpos;
+        //    y = (int)ypos;
+        //    if (r.x > p.X)
+        //    {
+        //        degx *= -1;
+        //    }
+        //    if (r.y > p.Y)
+        //    {
+        //        degy *= -1;
+        //    }
+        //    //if (r.x > p.X && r.y > p.Y)
+        //    //{
+        //    //   // MessageBox.Show("rx=>" + r.x + "__ry=>" + r.y + "__px=>" + p.X + "__py=>" + p.Y);
+        //    //    degx = -1;
+        //    //    degy = -1;
+        //    //}
+
+        //    //else if (r.x < p.X && r.y > p.Y)
+        //    //{
+        //    //    degx = Math.Abs(degx);
+        //    //    degy = -1;
+        //    //}
+        //    //else if (r.x > p.X && r.y < p.Y)
+        //    //{
+        //    //    degx = -1;
+        //    //    degy = Math.Abs(degy);
+        //    //}
+
+        //    dx =(double)p.X / 100;
+        //    dy =(double)p.Y / 100;
+
+        //}
+    }
+}
